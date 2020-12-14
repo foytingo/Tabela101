28dc53f88889d145bd686d68363a2695016173e2
Murat Baykor <foytingo@gmail.com>
2020-12-14 12:22:21 +0300

    Initial Commit

---
+++ Tabela101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabela101
+//
+//  Created by Murat Baykor on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
